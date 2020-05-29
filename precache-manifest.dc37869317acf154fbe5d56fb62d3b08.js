227843f9ba3a3f522577650f06fe07421198873b
kujebe <kujebeseun@gmail.com>
2020-05-29 03:43:46 +0200

    Updates

---
+++ precache-manifest.dc37869317acf154fbe5d56fb62d3b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c36372d4dfa4768265a897fe735c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a3389d13b640ae6c4b4",
+    "url": "/static/css/main.d3444550.chunk.css"
+  },
+  {
+    "revision": "d33b036509a900aaa81d",
+    "url": "/static/js/2.2b1fc8f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b1fc8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3389d13b640ae6c4b4",
+    "url": "/static/js/main.6d805b4a.chunk.js"
+  },
+  {
+    "revision": "b12780bc7b1372235439",
+    "url": "/static/js/runtime-main.01fb7b03.js"
+  }
+]);
