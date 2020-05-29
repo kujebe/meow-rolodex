bf85b4a5544072aa188ca4de3aba0f5309fb438a
kujebe <kujebeseun@gmail.com>
2020-05-29 03:11:23 +0200

    added custom font for h2.title

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>Cats Rolodex</h1>
+        <h1 className="title">Meow Rolodex</h1>
         <SearchBox
           placeholder="search monster"
           handleChange={this.onSearchChange}
